302ccc8c1a3e27d17c2203ac85558137d7626cc4
johnatan tavares <johnatan12014@hotmail.com>
2018-06-02 14:01:02 -0300

    Criando Models e Migrations State e City

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@
 Route::get('OneToOne', 'OneToOneController@oneToOne');
 Route::get('OneToOneInverse', 'OneToOneController@oneToOneInverse');
 Route::get('OneToOneInsert','OneToOneController@oneToOneInsert');
+
+Route::get('OneToMany','OneToManyController@oneToMany');

559afc7fcc46479f324d09234d08c1a7d9c6bc10
johnatan tavares <johnatan12014@hotmail.com>
2018-06-02 14:16:18 -0300

    One to Many

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\State;
 
 class OneToManyController extends Controller
 {
     public function oneToMany()
     {
-      
+      //Recuperando o país
+      $country = Country::where('name','Brasil')->get()->first();
+      //Recupera todos os estados relacionados com o país
+      $states = $country->states;
+      //Listando os estados
+      foreach ($states as $state) {
+        echo "Estado: ". $state->name . "<br>";
+      }
+
     }
+
+
+    
 }

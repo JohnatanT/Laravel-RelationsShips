1bd103335737066f6a76b5a462281443d7fdeb91
johnatan tavares <johnatan12014@hotmail.com>
2018-06-04 10:39:30 -0300

    Relation Polymorphic

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,17 @@
 
 */
 
+//One To One
 Route::get('OneToOne', 'OneToOneController@oneToOne');
 Route::get('OneToOneInverse', 'OneToOneController@oneToOneInverse');
 Route::get('OneToOneInsert','OneToOneController@oneToOneInsert');
-
+//One To Many
 Route::get('OneToMany','OneToManyController@oneToMany');
 Route::get('OneToManyTwo','OneToManyController@oneToManyTwo');
 Route::get('OneToManyInsert','OneToManyController@oneToManyInsert');
-
+//Many To Many
 Route::get('ManyToMany','ManyToManyController@manyToMany');
 Route::get('ManyToManyInverse','ManyToManyController@manyToManyInverse');
 Route::get('ManyToManyInsert','ManyToManyController@manyToManyInsert');
+//Relation Polymorphic
+Route::get('polymorphic','PolymorphicController@polymorphic');
